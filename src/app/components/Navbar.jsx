ceab315ebacaf608861ce92fdd3faece9817939e
Pawan Yadav <pawanydv3010@gmail.com>
2025-10-22 22:26:01 +0530

    navbar

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <nav>
+    <div className="menu ">
+        <a href="#">Home</a>
+        <a href="#">Dashboard</a>
+        <a href="#">Features</a>
+        <a href="#">About us</a>
+       
+    </div>
+   </nav>
+  );
+}
