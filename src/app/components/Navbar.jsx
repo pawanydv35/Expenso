9a412708eed6b00951a27ebac38e5e1de603edde
Pawan Yadav <pawanydv3010@gmail.com>
2025-11-02 09:30:33 +0530

    navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -6,11 +6,11 @@ export default function Home() {
   return (
    <nav className=" py-2">
       <ul className="flex justify-center ">
-        <li className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">Home</li>
-        <li className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">About</li>
-        <li className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">Contact us</li>
-        <li className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">Blogs</li>
-        <li className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">How it's work</li>
+        <li><a href="#"  className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">Home</a></li>
+        <li><a href="#"  className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">About</a></li>
+        <li><a href="#"  className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">Contact us</a></li>
+        <li><a href="#"  className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">Blogs</a></li>
+        <li><a href="#"  className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">How it works</a></li>
       </ul>
    </nav>
   );
