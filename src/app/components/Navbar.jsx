363f86e02a7b6d1e6c56e3cf3d0ad8656db5fc94
Pawan Yadav <pawanydv3010@gmail.com>
2025-11-02 09:21:41 +0530

    navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <nav>
-    <div className="menu ">
-        <a href="#">Home</a>
-        <a href="#">Dashboard</a>
-        <a href="#">Features</a>
-        <a href="#">About us</a>
-       
-    </div>
+   <nav className=" py-2">
+      <ul className="flex justify-center ">
+        <li className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">Home</li>
+        <li className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">About</li>
+        <li className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">Contact us</li>
+        <li className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">Blogs</li>
+        <li className="mx-5 hover:bg-green-300 hover:text-black rounded px-1">How it's work</li>
+      </ul>
    </nav>
   );
 }
