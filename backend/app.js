e1e9ada3341e8a71a5c6a746dc4e182b8d3c5a1c
Pawan Yadav <pawanydv3010@gmail.com>
2025-12-31 09:42:34 +0530

    routes

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,26 @@ app.get("/", (req,res)=>{
     res.send("hello express");
 });
 
+app.get("/dashboard", (req,res)=>{
+    res.send("hello express");
+});
+
+app.get("/features", (req,res)=>{
+    res.send("features");
+});
+
+app.get("/expense", (req,res)=>{
+    res.send("expense");
+});
+
+app.get("/about", (req,res)=>{
+    res.send("about me");
+});
+
+app.get("/contact", (req,res)=>{
+    res.send("contact us");
+});
+
 app.listen(8000, ()=>{
     console.log("server is running");
 });
